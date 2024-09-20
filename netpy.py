e99fbefb4ab8dd3f5f4833b04464ad2d92f152d0
Steven Yeu <stevenhanhyeu@gmail.com>
2024-09-20 10:27:02 -0700

    updated output to include diffs of params

--- netpy.py
+++ netpy.py
@@ -1,28 +1,39 @@
 import json
 import argparse
+from pprint import pp, pprint
 from typing import Any, Dict
 from os import listdir
 from os.path import isfile, join
+from deepdiff import DeepDiff
 
 from lib import apply_lsh
 
-CELL_PARAMS = ['popParams', 'cellParams', 'synMechParams', "connParams", "stimSourceParams", "stimTargetParams"]
+CELL_PARAMS = [
+    "popParams",
+    "cellParams",
+    "synMechParams",
+    "connParams",
+    "stimSourceParams",
+    "stimTargetParams",
+]
 
-def parse_netpyne_file(data_dict: Dict[str,Any]) -> Dict[str,str]:
+
+def parse_netpyne_file(data_dict: Dict[str, Any]) -> Dict[str, str]:
     result = {}
     for k in CELL_PARAMS:
-        value = json.dumps(data_dict[k],sort_keys=True)
-        result[k] = value 
+        value = json.dumps(data_dict[k], sort_keys=True)
+        result[k] = value
 
     return result
 
+
 def read_netpyne_file(file_path: str):
-    netpyne_data = open(file_path,'r')
+    netpyne_data = open(file_path, "r")
     netpyne_json = json.load(netpyne_data)
-    if 'net' not in netpyne_json:
+    if "net" not in netpyne_json:
         print(f"{file_path} is missing key 'net'")
         exit(1)
-    return netpyne_json['net']['params']
+    return netpyne_json["net"]["params"]
 
 
 def run():
@@ -30,30 +41,46 @@ def run():
 
     parser.add_argument("input")
     parser.add_argument("threshold")
+    parser.add_argument("--diff", default=True, action="store_true")
 
     args = parser.parse_args()
 
     files = [
         join(args.input, f) for f in listdir(args.input) if isfile(join(args.input, f))
     ]
-    file_params = { f: parse_netpyne_file(read_netpyne_file(f)) for f in files}
-
+    file_params = {f: parse_netpyne_file(read_netpyne_file(f)) for f in files}
     result = {}
-
+    data = []
     for param in CELL_PARAMS:
-        data = [ (filename,data_dict[param]) for filename, data_dict in file_params.items()]
+        data = [
+            (filename, data_dict[param]) for filename, data_dict in file_params.items()
+        ]
         res, pairs = apply_lsh(data, args.threshold)
-        result[param] = (res,pairs)
+        result[param] = (res, pairs)
 
     for params, res in result.items():
         print(f"Results for {params}")
         print(res[0])
-        print(res[1])
-        print()
-    
+        for (
+            file1,
+            file2,
+            score,
+        ) in res[1]:
+            print(f"{file1} and {file2} have a similarity score of {score}")
+            if abs(float(score) - 1.0) > 0.0001:
+                f1 = json.loads(file_params[file1][params])
+                f2 = json.loads(file_params[file2][params])
+                print(f"Data for {file1}")
+                pprint(f1, indent=4, width=80, depth=4)
+                print()
+                print(f"Data for {file2}")
+                pprint(f2, indent=4, width=80, depth=4)
+                print()
+                print(f"Differences for {params}")
+                param_diff = DeepDiff(f1, f2, ignore_order=True)
+                pprint(param_diff, indent=4, width=80, depth=4)
 
-    
 
 
 if __name__ == "__main__":
-   run() 
+    run()
