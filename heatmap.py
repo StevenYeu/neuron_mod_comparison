3bf8c7724168778cd3efbe4c52e4da2b926a3ea9
Steven Yeu <stevenhanhyeu@gmail.com>
2025-05-15 10:22:29 -0700

    updated heatmap script to plot all inhib to excit conn params

--- heatmap.py
+++ heatmap.py
@@ -4,50 +4,94 @@ import re
 import pandas as pd
 import seaborn as sns
 import matplotlib.pylab as plt
+from enum import Enum
 
-CONN_TYPE = "SOM_PT_HH_full"
+CONN_TYPES = set(
+    [
+        "SOM_IT_HH_full",
+        "SOM_PT_HH_full",
+        "SOM_CT_HH_full",
+        "PV_IT_HH_full",
+        "PV_PT_HH_full",
+        "PV_CT_HH_full",
+        "VIP_IT_HH_full",
+        "VIP_PT_HH_full",
+        "VIP_CT_HH_full",
+        "NGF_IT_HH_full",
+        "NGF_PT_HH_full",
+        "NGF_CT_HH_full",
+    ]
+)
+
+INHIBITORY_CELL = {"SOM": 0, "PV": 1, "VIP": 2, "NGF": 3}
+EXCITATORY_CELL = {"IT": 0, "PT": 1, "CT": 2}
+
+MAX_ROW = 12
+MAX_COL = 9
+OFF_SET = 3
+
+
+class CellType(Enum):
+    INHIBITORY = 1
+    EXCITATORY = 2
+
+
+def generate_label(cell_type: CellType) -> list[str]:
+    labels = []
+    cells = None
+    if cell_type == CellType.INHIBITORY:
+        cells = INHIBITORY_CELL
+    else:
+        cells = EXCITATORY_CELL
+
+    for cell in cells.keys():
+        for i in range(3):
+            label = f"{cell} layer {i}"
+            labels.append(label)
+    return labels
 
 
 def load_connParams(file_path: str) -> NDArray[np.float64]:
+    data = np.zeros((MAX_ROW, MAX_COL))
     conn_params = pd.read_json(file_path)
-    som_pt = {key: value.to_dict() for key, value in conn_params.items() if CONN_TYPE in key}
 
-    som_max_layer = 0
-    pt_max_layer = 0
+    conn = {}
 
-    for key in som_pt.keys():
+    for conn_type in CONN_TYPES:
+        for key, value in conn_params.items():
+            if conn_type in key:
+                conn[key] = value.to_dict()
+
+    for key, value in conn.items():
+        cells = key.split("_")
+        in_cell = cells[0]
+        ex_cell = cells[1]
         layers = re.findall("\d+", key)
-        if len(layers) > 2:
-            print(f"Found {len(layers)} layers")
-        som_max_layer = max(som_max_layer, int(layers[0]))
-        pt_max_layer = max(pt_max_layer, int(layers[1]))
-
-    print(f"Max SOM layer is {som_max_layer + 1}")
-    print(f"Max PT layer is {pt_max_layer + 1}")
-
-    data = np.zeros((som_max_layer + 1, pt_max_layer + 1))
-
-    for key, value in som_pt.items():
-        layers = re.findall("\d+", key)
-        print(value)
-        som_layer = int(layers[0])
-        pt_layer = int(layers[1])
-        if len(layers) > 2:
-            print(f"Found {len(layers)} layers")
-        data[som_layer][pt_layer] = value["weight"]
-
-    print(data)
+        in_index = (OFF_SET * INHIBITORY_CELL[in_cell]) + int(layers[0])
+        ex_index = (OFF_SET * EXCITATORY_CELL[ex_cell]) + int(layers[1])
+        data[in_index][ex_index] = value["weight"]
 
     return data
 
 
 def main():
+    inhibtory_cell_labels = generate_label(CellType.INHIBITORY)
+    excitatory_cell_labels = generate_label(CellType.EXCITATORY)
     data = load_connParams("./m1/v103_connParams.json")
-    sns.heatmap(data, cmap="YlGnBu")
+    graph = sns.heatmap(
+        data,
+        cmap="YlGnBu",
+        xticklabels=excitatory_cell_labels,
+        yticklabels=inhibtory_cell_labels,
+    )
+    graph.set_title("M1 v103 Conn Params")
+    graph.xaxis.tick_top()
+    graph.xaxis.set_label_position("top")
 
-    plt.title("SOM to PT")
-    plt.savefig('m1_v103_som_pt.png')
-    # plt.show()
+    # plt.xlabel("Excitatory Cells")
+    # plt.ylabel("Inhibitory Cells")
+
+    plt.show()
 
 
 if __name__ == "__main__":
